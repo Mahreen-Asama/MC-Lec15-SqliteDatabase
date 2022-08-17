e6274724e5ee746db0b52fd5e1172878d8ea2705
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-08-17 20:23:54 +0500

    DB classes added

---
+++ app/src/main/java/com/example/lec15_sqlitedatabase/DBHelper.java
@@ -0,0 +1,102 @@
+package com.example.lec15_sqlitedatabase;
+
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String STUDENT_ID = "StudentID";
+    public static final String STUDENT_NAME = "StudentName";
+    public static final String STUDENT_ROLL = "StudentRollNumber";
+    public static final String STUDENT_ENROLL = "IsEnrolled";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+    //constructor
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, "MyDB.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //String createTableSTatementOne = "CREATE TABLE CustTable(CustomerID Integer PRIMARY KEY AUTOINCREMENT, " + CUSTOMER_NAME_FIRST + " Text, CustomerAge Int, ActiveCustomer BOOL) ";
+        String createTableSTatement = "CREATE TABLE " + STUDENT_TABLE + "(" +
+                STUDENT_ID + " Integer PRIMARY KEY AUTOINCREMENT, " + STUDENT_NAME + " Text, "
+                + STUDENT_ROLL + " Int, " + STUDENT_ENROLL + " BOOL) ";
+        db.execSQL(createTableSTatement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
+        onCreate(db);
+    }
+
+    //---------------CRUD--------------
+    //ADD
+    public void  addStudent(StudentModel STUDENTModel){
+        SQLiteDatabase db = this.getWritableDatabase();         //getting object of Db
+        //Hash map, as we did in bundles
+        ContentValues cv = new ContentValues();
+
+        cv.put(STUDENT_NAME, STUDENTModel.getName());
+        cv.put(STUDENT_ROLL, STUDENTModel.getRollNmber());
+        cv.put(STUDENT_ENROLL, STUDENTModel.isEnroll());
+
+        db.insert(STUDENT_TABLE, null, cv);
+
+        db.close();
+        //NullCoumnHack
+        //long insert =
+        //if (insert == -1) { return false; }
+        //else{return true;}
+    }
+    //Update
+    public void updateStudent(int id, StudentModel STUDENTModel){
+        ContentValues cv = new ContentValues();
+
+        cv.put(STUDENT_NAME, STUDENTModel.getName());
+        cv.put(STUDENT_ROLL, STUDENTModel.getRollNmber());
+        cv.put(STUDENT_ENROLL, STUDENTModel.isEnroll());
+
+        SQLiteDatabase db = this.getWritableDatabase();         //getting object of Db
+        //Cursor cursorCourses = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, STUDENT_ID:id);
+
+    }
+    //Delete
+    public void deleteStudent(int id){
+        SQLiteDatabase db = this.getWritableDatabase();         //getting object of Db
+        //Cursor cursorCourses = db.rawQuery("DELETE FROM " + STUDENT_TABLE, STUDENT_ID==id);
+
+    }
+    //READ
+    public ArrayList<StudentModel> getAllStudents() {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursorCourses = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, null);
+
+        ArrayList<StudentModel> studentArrayList = new ArrayList<>();
+
+        // moving our cursor to first position.
+        if (cursorCourses.moveToFirst()) {
+            do {
+
+                studentArrayList.add(new StudentModel(cursorCourses.getString(1),
+                        cursorCourses.getInt(2),
+                        cursorCourses.getInt(3) == 1 ? true : false));
+            } while (cursorCourses.moveToNext());
+
+        }
+
+        cursorCourses.close();
+        db.close();
+        return studentArrayList;
+    }
+
+}
---
+++ app/src/main/java/com/example/lec15_sqlitedatabase/StudentModel.java
@@ -0,0 +1,49 @@
+package com.example.lec15_sqlitedatabase;
+
+import androidx.annotation.NonNull;
+
+public class StudentModel {
+    private String name;
+    private int rollNmber;
+    private boolean isEnroll;
+
+    public StudentModel(String name, int rollNmber, boolean isEnroll) {
+        this.name = name;
+        this.rollNmber = rollNmber;
+        this.isEnroll = isEnroll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNmber() {
+        return rollNmber;
+    }
+
+    public void setRollNmber(int rollNmber) {
+        this.rollNmber = rollNmber;
+    }
+
+    public boolean isEnroll() {
+        return isEnroll;
+    }
+
+    public void setEnroll(boolean enroll) {
+        isEnroll = enroll;
+    }
+
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "name='" + name + '\'' +
+                ", rollNmber=" + rollNmber +
+                ", isEnroll=" + isEnroll +
+                '}';
+    }
+}
