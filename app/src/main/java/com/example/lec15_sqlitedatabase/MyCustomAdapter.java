4a4dc7588fdbed5387bb3152ecc0586c9ef13527
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-08-19 01:03:03 +0500

    custom lv start

---
+++ app/src/main/java/com/example/lec15_sqlitedatabase/MyCustomAdapter.java
@@ -0,0 +1,23 @@
+package com.example.lec15_sqlitedatabase;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class MyCustomAdapter extends ArrayAdapter<StudentModel> {
+    public MyCustomAdapter(@NonNull Context context, int resource, @NonNull List<StudentModel> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
